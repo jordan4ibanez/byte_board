cd05885a11e44524d804a7d31b1714655335d47d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-12-16 02:39:57 -0500

    Implement this disaster

--- source/app.d
+++ source/app.d
@@ -1,20 +1,65 @@
 import std.stdio;
+import std.conv;
+import std.string: isNumeric;
+import std.algorithm.comparison: equal;
 
 void main() {
+
     bool quit = false;
-    while (!quit) {
+
+    string accumulator = "";
+
+    outer: while (!quit) {
 
         write("Input your bits: ");
 
+        ubyte query = 0;
+
         try {
 
-            char[] input = readln();
+            string input = readln();
 
-            
+            if (equal(input, "quit\n")) {
+                break outer;
+            }
+
+            char[] blah = input.dup;
+
+            // Wow we're really turning up the optimizations here
+
+            foreach (number; 1..9) {
+
+                inner: foreach (letter; blah) {
+
+                    string sample = to!string(letter);
+
+                    if (isNumeric(sample)) {
+
+                        if (to!int(sample) == number) {
+
+                            // Simple register shifting
+
+                            ubyte shift = cast(ubyte)(8 - number);
+
+                            query |= 1 << shift;
+
+                            break inner;
+                        }
+
+                    }    
+
+                }
+            }
             
 
         } catch (Exception e) {
             writeln("You done goofed, try again");
         }
+        
+        accumulator ~= cast(char)query;
+
+        writeln("\nCurrent sentence: ", accumulator, "\n");
     }
+
+    writeln("See ya!");
 }
